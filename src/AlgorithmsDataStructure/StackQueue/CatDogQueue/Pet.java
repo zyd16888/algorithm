ec4b31df69d8ddd4a3b727e70a3a8a7a08978959
zyd16888 <zyd475182659@163.com>
2019-01-17 16:36:25 +0800

    猫狗队列，具体问题在DogCatQueue

---
+++ src/AlgorithmsDataStructure/StackQueue/CatDogQueue/Pet.java
@@ -0,0 +1,24 @@
+package AlgorithmsDataStructure.StackQueue.CatDogQueue;
+
+/**
+ * 描述:
+ * 宠物类
+ *
+ * @author dong
+ * @date 2019-01-16 17:02
+ */
+public class Pet {
+    private String type;
+
+    public Pet(String type) {
+        this.type = type;
+    }
+
+    public String getPetType() {
+        return type;
+    }
+
+    public void setPetType(String type) {
+        this.type = type;
+    }
+}
