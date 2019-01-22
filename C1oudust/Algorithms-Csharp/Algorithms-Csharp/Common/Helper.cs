85c44cd754895ce684b3f25280dff7d1192ed212
C1oudust <amerkiousy@gmail.com>
2019-01-22 18:05:00 +0800

    something change

---
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/Common/Helper.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithms_Csharp.Common
+{
+    public  static class Helper
+    {
+        public static void Swap(int[] arr, int firstIndex, int secondIndex)
+        {
+            if (arr.Length < 2 ||  firstIndex == secondIndex)
+                return;
+            arr[firstIndex] = arr[firstIndex] ^ arr[secondIndex];
+            arr[secondIndex] = arr[firstIndex] ^ arr[secondIndex];
+            arr[firstIndex] = arr[firstIndex] ^ arr[secondIndex];
+        }
+    }
+}
