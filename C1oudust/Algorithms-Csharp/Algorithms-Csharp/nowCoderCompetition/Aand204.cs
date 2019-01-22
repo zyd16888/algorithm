2b6111d0f28fe6ca649fdf03a73ac91dec2b76ab
C1oudust <amerkiousy@gmail.com>
2019-01-22 20:36:28 +0800

    something changed

--- C1oudust/Algorithms-Csharp/Algorithms-Csharp/nowCoderCompetition/Aand204.cs
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/nowCoderCompetition/Aand204.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+链接：https://ac.nowcoder.com/acm/contest/317/B
+题目描述：
+小a非常喜欢204这个数字，因为′a′+′k′=204′a′+′k′=204。现在他有一个长度为n的序列，其中只含有2,0,4这三种数字，设ai为序列中第i个数，
+你需要重新排列这个数列，使得∑i=1n(ai−ai−1)^2最大(公式的含义是：每个数与前一个数差的平方的和)注意：我们默认a0=0
 
-namespace Algorithms_Csharp.newCodeCompetition
+输入描述:第一行一个整数n接下来一行n个整数，第i个数表示ai
+
+输出一个整数，表示∑i=1n(ai−ai−1)^2的最大值
+
+eg: 
+Input:2
+    2 4
+Output:
+    20
+解释：按(4,2)排列是最优的，此时sum=(4−0)^2+(2−4)^2=20
+*/
+using System;
+
+namespace Algorithms_Csharp.nowCoderCompetition
 {
     class Aand204
     {
---
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/nowCoderCompetition/AandCalculator.cs
@@ -0,0 +1,71 @@
+﻿/*
+ 链接：https://ac.nowcoder.com/acm/contest/317/A
+题目描述:
+小a的数学基础实在太差了，以至于他只会用计算器算数。他的计算器比较特殊，只有+,−,×,/+,−,×,/(即加减乘除)四种运算。
+经过一番周折，小a终于算出了他想要的数，但是他却忘记了最初的数是什么。不过幸运的是他记下了整个操作序列，他想请你帮他算出最初的数
+
+输入描述:第一行两个整数n,Xn,X，分别表示操作次数和最终的数接下来nn行表示操作序列，每行两个数opt,x
+若opt=1,则表示将当前数加xx
+若opt=2，则表示将当前数减x
+若opt=3，则表示将当前数乘x
+若opt=4，则表示将当前数除以x
+
+输出描述:
+一个整数表示最初的数
+eg:
+Input:
+4 6
+1 3
+2 1
+3 3
+4 2
+Output:
+2
+
+解释：2+3=5 5−1=4 4∗3=12 12/2=6
+ */
+using System;
+
+namespace Algorithms_Csharp.nowCoderCompetition
+{
+    class AandCalculator
+    {
+        public static void Run()
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            long res = Int32.Parse(Console.ReadLine());
+            int[] steps = new int[n * 2];
+
+            for (int i = 0; i < steps.Length - 1; i += 2)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                steps[i] = Int32.Parse(s[0]);
+                steps[i + 1] = Int32.Parse(s[1]);
+            }
+
+            for (int i = steps.Length - 1; i >= 0; i--)
+            {
+                if (steps[i - 1] == 1)
+                {
+                    res = res - steps[i];
+                }
+                else if (steps[i - 1] == 2)
+                {
+                    res = res + steps[i];
+                }
+                else if (steps[i - 1] == 3)
+                {
+                    res = res / steps[i];
+                }
+                else if (steps[i - 1] == 4)
+                {
+                    res = res * steps[i];
+                }
+
+                i--;
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
