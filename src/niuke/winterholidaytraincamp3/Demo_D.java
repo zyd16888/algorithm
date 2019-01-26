181fe1447bea44052b932c96dc7dd5c2e816fd15
zyd16888 <zyd475182659@163.com>
2019-01-26 14:53:36 +0800

    二维数组遍历

---
+++ src/niuke/winterholidaytraincamp3/Demo_D.java
@@ -0,0 +1,56 @@
+package niuke.winterholidaytraincamp3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * 描述:
+ * 处女座的训练
+ * https://ac.nowcoder.com/acm/contest/329/D
+ *
+ * @author dong
+ * @date 2019-01-26 14:00
+ */
+public class Demo_D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+        for (int i=0; i<n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        Arrays.sort(arr, new Sort());               //按疲惫值由大到小排序
+        System.out.println("------------------");
+        for (int i=0; i<n; i++){
+            System.out.print(arr[i][0]+" ");
+        }
+        System.out.println();
+        for (int i=0; i<n; i++){
+            System.out.print(arr[i][1]+" ");
+        }
+        System.out.println();
+        System.out.println("------------------");
+        int fin = 0;
+        for (int i=0; i<n; i++){        // 做题顺序
+            int num = 0;
+            for (int j = i+1; j<n; j++){    //计算单位疲劳时间
+                num += arr[j][1];
+            }
+            fin += num * arr[i][0];
+        }
+        System.out.println(fin);
+
+    }
+
+
+}
+
+class Sort implements Comparator<int[]>{
+
+    @Override
+    public int compare(int[] o1, int[] o2) {
+        return o2[1] - o1[1];
+    }
+}
