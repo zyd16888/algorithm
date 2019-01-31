0d0348a8c941b494272c1ce8df202c3a74e9c090
C1oudust <amerkiousy@gmail.com>
2019-01-31 19:53:23 +0800

    add some news

---
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/nowCoderCompetition/Day-5/CoolMath.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * https://ac.nowcoder.com/acm/contest/331/J
+ */
+using System;
+
+namespace Algorithms_Csharp.nowCoderCompetition.Day_5
+{
+    class CoolMath
+    {
+        public static void Run()
+        {
+            int m = int.Parse(Console.ReadLine());
+            long ans = 1;
+            for (int i = 0; i < m; i++)
+            {
+                ans *= 3;
+                ans %= 998244353;
+            }
+
+            Console.WriteLine(ans);
+
+
+        }
+
+    }
+}
---
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/nowCoderCompetition/Day-5/CoolNumber.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * https://ac.nowcoder.com/acm/contest/331/G
+ */
+using System;
+
+namespace Algorithms_Csharp.nowCoderCompetition.Day_5
+{
+    class CoolNumber
+    {
+        public static void Run()
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] c = new int[n];
+            int[] a = new int[1000005];
+            int[] ans = new int[1000005];
+
+            for (int i = 1; i <= 1000000; i++)
+            {
+                for (int j = i; j <= 1000000; j += i)
+                {
+                    a[j]++;
+                }
+                if (ans[a[i]] == 0) ans[a[i]] = i;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                c[j] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var v in c)
+            {
+                Console.WriteLine(ans[v]);
+            }
+
+        }
+        //            int n = Convert.ToInt32(Console.ReadLine());
+        //            int[] c = new int[n];
+        //            for (int j = 0; j < n; j++)
+        //            {
+        //                int num = int.Parse(Console.ReadLine());
+        //                c[j] = Find(num);
+        //            }
+        //
+        //            foreach (var i in c)
+        //            {
+        //                Console.WriteLine(i);
+        //            }
+
+    
+
+        public static int Find(int n)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                if (Factor(i, n))
+                    return i;
+            }
+            return -1;
+        }
+        public static bool Factor(int n, int num)
+        {
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+
+                }
+            }
+            if (count == num)
+                return true;
+            return false;
+        }
+    }
+}
