78a554592b65e0cbdf145b1946d0e01d4c27bbb3
C1oudust <amerkiousy@gmail.com>
2019-02-02 20:21:58 +0800

    day-6

---
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/nowCoderCompetition/Day-6/GasRange.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * https://ac.nowcoder.com/acm/contest/332/B
+ */
+using System;
+
+namespace Algorithms_Csharp.nowCoderCompetition.Day_6
+{
+    class GasRange
+    {
+        public static void Run()
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            long n = long.Parse(s[0]);
+            long m = long.Parse(s[1]);
+            long d = long.Parse(s[2]);
+            long x = long.Parse(s[3]);
+            long day = 0;
+            long sal = 0;
+            while (sal < m && day < x)
+            {
+                day++;
+                sal += n;
+                n += d;
+            }
+            Console.WriteLine(day);
+
+        }
+    }
+}
