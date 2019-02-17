49682cbf2509f692ce391f805437818319e35225
C1oudust <amerkiousy@gmail.com>
2019-02-17 23:05:28 +0800

    two of array

---
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/Array/FirstNotRepeat.cs
@@ -0,0 +1,27 @@
+﻿//找到数组中第一个不重复的数
+//时间复杂度O(n),辅助的空间复杂度O(n)。
+using System.Collections.Generic;
+
+namespace Algorithms_Csharp.Array
+{
+    class FirstNotRepeat
+    {
+        public int FindFirstNotRepeat(int[] a)
+        {
+            Dictionary<int,int> temp = new Dictionary<int, int>();
+            foreach (var val in a)
+            {
+                temp[val]++;
+            }
+
+            foreach (var val in a)
+            {
+                if (temp[val] == 1)
+                {
+                    return val;
+                }               
+            }
+            return 0;
+        }
+    }
+}
---
+++ C1oudust/Algorithms-Csharp/Algorithms-Csharp/Array/SecondSmallest.cs
@@ -0,0 +1,29 @@
+﻿//找到数组中第二小的数
+//时间复杂度O(n)
+using System;
+
+namespace Algorithms_Csharp.Array
+{
+    class SecondSmallest
+    {
+        int firstMin = Int32.MaxValue;
+        int secondMin = Int32.MaxValue;
+
+        public int FindSecondSmallest(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < firstMin)
+                {
+                    secondMin = firstMin;
+                    firstMin = a[i];
+                }
+                else if (a[i]< secondMin && a[i] != firstMin)
+                {
+                    secondMin = a[i];
+                }
+            }
+            return secondMin;
+        }
+    }
+}
