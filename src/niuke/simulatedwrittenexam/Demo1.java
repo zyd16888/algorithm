0115f7cf18324421893b65cb8759a16e4c92ae50
zyd16888 <zyd475182659@163.com>
2019-02-25 10:35:41 +0800

    牛客网春招模拟笔试

---
+++ src/niuke/simulatedwrittenexam/Demo1.java
@@ -0,0 +1,26 @@
+package niuke.simulatedwrittenexam;
+
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        long f = sc.nextLong();
+        long d = sc.nextLong();
+        long p = sc.nextLong();
+        int t = 0;
+        if (x>d){
+            System.out.println("0");
+        }else {
+            long all = x*t + (t-f)*p ;
+            while (all<d){
+                t++;
+                all = x*t + (t-f)*p ;
+            }
+        }
+
+        System.out.println(t-1);
+
+    }
+}
