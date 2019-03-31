68fee2de2c4284095b9e2f8cdc043bb8c227d34d
zyd16888 <zyd475182659@163.com>
2019-03-31 21:57:37 +0800

    牛客网模拟笔试3月场

---
+++ niuke/src/simulatedwrittenexamMar/Demo1.java
@@ -0,0 +1,21 @@
+package simulatedwrittenexamMar;
+
+import java.util.Scanner;
+
+/**
+ * 描述:
+ *
+ * @author dong
+ * @create 2019-03-14 19:40
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+        sc.close();
+
+
+
+    }
+}
