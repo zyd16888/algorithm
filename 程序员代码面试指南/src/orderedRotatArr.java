f813d649f98231add08bb48be95a55d2119dfd15
zyd16888 <zyd475182659@163.com>
2019-09-03 10:59:25 +0800

    在有序旋转数组中寻找一个数

--- 程序员代码面试指南/src/orderedRotatArr.java
+++ 程序员代码面试指南/src/orderedRotatArr.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 /**
  * @author dong
  * @date 2019/09/01 11:05
- * TODO: 有序旋转数组寻找最小值
+ * TODO: 有序旋转数组
  */
 public class orderedRotatArr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int num = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.println(getMin(arr));
+        System.out.println(isContains(arr, num) ? "Yes" : "No");
+        //System.out.println(getMin(arr));
 
     }
 
+    /**
+     *  TODO: 有序旋转数组寻找最小值
+     * @param arr
+     * @return
+     */
     public static int getMin(int[] arr){
         int low = 0;
         int high = arr.length -1;
@@ -49,5 +56,62 @@ public class orderedRotatArr {
             }
         }
         return Math.min(arr[low], arr[high]);
+    }
+
+    /**
+     * TODO: 在有序旋转数组中寻找一个数
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static boolean isContains(int[] arr, int num){
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = 0;
+        while (low <= high){
+            mid = (low + high) / 2;
+            if (arr[mid] == num){
+                return true;
+            }
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]){
+                while (low != mid && arr[low] == arr[mid]){
+                    low ++;
+                }
+                if (low == mid){
+                    low = mid + 1;
+                    continue;
+                }
+            }
+            if (arr[low] != arr[mid]){
+                if (arr[mid] > arr[low]){
+                    if (num >= arr[low] && num < arr[mid]){
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                } else {
+                    if (num > arr[mid] && num <= arr[high]){
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                }
+            } else {
+                if (arr[mid] < arr[high]){
+                    if (num > arr[mid] && num <= arr[high]){
+                        low  = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                } else {
+                    if (num >= arr[mid] && num < arr[mid]){
+                        high = mid - 1;
+                    } else {
+                        low  = mid + 1;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
