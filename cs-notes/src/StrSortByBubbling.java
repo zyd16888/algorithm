2a4acd894825cfa5e89355c201e0ca0d8a1d13a8
zyd16888 <zyd475182659@163.com>
2020-03-07 21:07:45 +0800

    字符串转为字符，通过冒泡排序

---
+++ cs-notes/src/StrSortByBubbling.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * 描述:
+ * 字符串排序
+ * 将字符串转为char类型，然后进行冒泡排序
+ *
+ * @author dong
+ * @date 2020-03-07 19:39
+ */
+public class StrSortByBubbling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] arr = str.toCharArray();
+        char temp = 0;
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr.length-1; j++){
+                if (arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for (char a :
+                arr) {
+            System.out.print(a);
+        }
+
+    }
+}
