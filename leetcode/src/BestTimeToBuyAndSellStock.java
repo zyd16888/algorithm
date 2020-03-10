d33d9810478aab0f60c4515652cc08afff5ec1b1
zyd16888 <zyd475182659@163.com>
2020-03-10 10:21:26 +0800

    121.买卖股票的最佳时机

---
+++ leetcode/src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,21 @@
+
+/**
+ * 描述:
+ * 121.买卖股票的最佳时机
+ *
+ * @author dong
+ * @date 2020-03-10 10:01
+ */
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1){
+            return 0;
+        }
+        int min = prices[0], max = 0;
+        for (int i = 1; i < prices.length; i++){
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+}
