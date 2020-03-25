09956adf924ff7d87acecf27ad7b095a564b44c1
zyd16888 <zyd475182659@163.com>
2020-03-25 22:49:59 +0800

    feat：892. surface area of 3d shapes

---
+++ leetcode/src/simple/SurfaceAreaOf3DShapes.java
@@ -0,0 +1,28 @@
+package simple;
+
+/**
+ * 描述:
+ * 892. 三维形体的表面积
+ *
+ * @author dong
+ * @date 2020-03-25 14:58
+ */
+public class SurfaceAreaOf3DShapes {
+    public int surfaceArea(int[][] grid) {
+        int n = grid.length, area = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int level = grid[i][j];
+                if (level > 0) {
+                    // 一个柱体中：2个底面 + 所有的正方体都贡献了4个侧表面积
+                    area += (level << 2) + 2;
+                    // 减掉 i 与 i-1 相贴的两份表面积
+                    area -= i > 0 ? Math.min(level, grid[i - 1][j]) << 1: 0;
+                    // 减掉 j 与 j-1 相贴的两份表面积
+                    area -= j > 0 ? Math.min(level, grid[i][j - 1]) << 1: 0;
+                }
+            }
+        }
+        return area;
+    }
+}
