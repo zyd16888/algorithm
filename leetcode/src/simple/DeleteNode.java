a106d2e62076d04ae76caf79bc2c3807e9652955
zyd16888 <zyd475182659@163.com>
2020-04-24 07:04:10 +0800

    feat：删除链表中的节点

---
+++ leetcode/src/simple/DeleteNode.java
@@ -0,0 +1,25 @@
+package simple;
+
+/**
+ * 描述:
+ * 删除链表的节点
+ *
+ * @author dong
+ * @date 2020-04-24 6:53
+ */
+
+public class DeleteNode {
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode node = new ListNode(0);
+        node.next = head;
+        ListNode cur = node;
+        while (cur != null && cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return node.next;
+    }
+}
