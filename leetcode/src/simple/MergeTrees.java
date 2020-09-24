88a188fc52e81aa31d0e57068ee1b19ad7235f44
zyd16888 <zyd475182659@163.com>
2020-09-24 08:41:13 +0800

    617. 合并二叉树

---
+++ leetcode/src/simple/MergeTrees.java
@@ -0,0 +1,23 @@
+package simple;
+
+/**
+ * 617. 合并二叉树
+ *
+ * @author dong
+ * @date 2020-09-23 8:15
+ */
+
+public class MergeTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode((t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val));
+
+        root.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        root.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+
+        return root;
+    }
+}
