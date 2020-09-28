71f0bcff61f189e1005dceb02d7a4f7ecccc1408
zyd16888 <zyd475182659@163.com>
2020-09-28 08:18:12 +0800

    node 树节点

---
+++ leetcode/src/simple/Node.java
@@ -0,0 +1,28 @@
+package simple;
+
+/**
+ * 描述：
+ * node 树节点
+ *
+ * @author zyd47
+ * @date 2020-09-28 8:14
+ */
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
