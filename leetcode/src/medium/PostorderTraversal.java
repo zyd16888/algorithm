cb88e8b2f1b2aeb8cdcc84745461db593bfbd7e3
zyd16888 <zyd475182659@163.com>
2021-01-07 11:21:02 +0800

    145. 二叉树的后序遍历

---
+++ leetcode/src/medium/PostorderTraversal.java
@@ -0,0 +1,26 @@
+package medium;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 描述：
+ * 145. 二叉树的后序遍历
+ *
+ * @author zyd47
+ * @date 2020-09-29 8:14
+ */
+public class PostorderTraversal {
+    public List<Integer> res = new ArrayList<Integer>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null){
+            return res;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        res.add(root.val);
+        return res;
+    }
+}
