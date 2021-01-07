519e31fcfad59075c9659f751906bc3b44a17477
zyd16888 <zyd475182659@163.com>
2021-01-07 11:21:19 +0800

    69. x的平方根

---
+++ leetcode/src/simple/MySqrt.java
@@ -0,0 +1,19 @@
+package simple;
+
+/**
+ * 描述：
+ * 69. x的平方根
+ *
+ * @author zyd47
+ * @date 2020-09-28 16:13
+ */
+public class MySqrt {
+    public int mySqrt(int x) {
+        int res = (int) Math.sqrt(x);
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
