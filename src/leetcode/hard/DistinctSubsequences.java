b738e13682252a7feecf555151b880a5d4cdccb9
zyd16888 <zyd475182659@163.com>
2021-03-22 11:46:11 +0800

    115. 不同的子序列

---
+++ src/leetcode/hard/DistinctSubsequences.java
@@ -0,0 +1,30 @@
+package leetcode.hard;
+
+/**
+ * 描述：
+ * 115. 不同的子序列
+ *
+ * @author dong
+ * @date 2021-03-17 09:54
+ */
+public class DistinctSubsequences {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (t.charAt(i - 1) == s.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
